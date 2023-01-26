fe6621e23fd063a3b337a1ee252d447d6b1bd5dc
gmpero <72013308+gmpero@users.noreply.github.com>
2023-01-26 09:17:32 +0300

    Update counter-02.js

--- rolli-shop-start-JSproject/js/counter-02.js
+++ rolli-shop-start-JSproject/js/counter-02.js
@@ -1,27 +1,26 @@
 // Добавляем прослушку на всем окне
 window.addEventListener('click', function(event){
+    let counter;
+    // Проверяем является ли элемент кнопкой плюс или минус
+    if(event.target.dataset.action === 'plus' || event.target.dataset.action === 'minus')
+    {
+        // находим родитель элемента и присваиваем в переменную
+        const counterWrapper = event.target.closest('.counter-wrapper');
+        // у родителя элемента находим нужный нам блок по дата атрибуту и записываем значение в переменную
+        counter = counterWrapper.querySelector('[data-counter]');
+    }
     // Проверяем является ли элемент кнопкой плюс
     if(event.target.dataset.action === 'plus')
-    {
-        const counterWrapper = event.target.closest('.counter-wrapper');
-        console.log(counterWrapper);
-
-        const counter = counterWrapper.querySelector('[data-counter]');
-        //console.log(counter);
+    {    
         counter.innerText = ++counter.innerText;
     }
     // Проверяем является ли элемент кнопкой минус
     if(event.target.dataset.action === 'minus')
     {
-        const counterWrapper = event.target.closest('.counter-wrapper');
-        console.log(counterWrapper);
-
-        const counter = counterWrapper.querySelector('[data-counter]');
-        
-        if(parseInt(counter.innerText) > 1){
-            //console.log(counter);
+        if(parseInt(counter.innerText) > 1)
+        {
             counter.innerText = --counter.innerText;
         }
     }
-
+    
 })
